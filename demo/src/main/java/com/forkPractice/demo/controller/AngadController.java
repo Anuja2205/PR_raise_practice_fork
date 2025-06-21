6cf962fc5ee50d9314b466589c52e08cc237dd3f
Angad Raut <anadraut6493@gmail.com>
2025-06-21 16:28:07 +0530

    say hello angad controller

--- demo/src/main/java/com/forkPractice/demo/controller/AngadController.java
+++ demo/src/main/java/com/forkPractice/demo/controller/AngadController.java
@@ -1,9 +1,14 @@
 package com.forkPractice.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@
+@RequestMapping("/api/angad")
 public class AngadController {
-
+    @GetMapping("/say-hello")
+    public String gretting(){
+        return "Hello!!!";
+    }
 }
